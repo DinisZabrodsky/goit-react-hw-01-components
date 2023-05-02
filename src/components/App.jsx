3d31f044984b7817a5ef20acc7d2dd023f7db644
Dinis PC <DinisZabrodsky@gmail.com>
2023-05-02 21:29:52 +0300

    іефешіешсі сщьздфеу

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,29 @@
 import user from "./Profile/user.json" 
+import statisticsData from "./Statistics/data.json"
 
 import {Profile}  from "./Profile/Profile";
+import {Statistics} from './Statistics/Statistics';
+
+import appCss from './App.module.css'
 
 
 
 export const App = () => {
-  return<>
-  <Profile         
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  followers={user.stats.followers}
-  views={user.stats.views}
-  likes={user.stats.likes} 
-/>
-</> 
+  return<div className={appCss.conteiner}>
+  <Profile        
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    followers={user.stats.followers}
+    views={user.stats.views}
+    likes={user.stats.likes} 
+  />
+
+  <Statistics 
+    titel="Upload stats"
+    data={statisticsData}
+  />
+
+</div> 
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,21 @@
+// import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import statCss from './Statistics.module.css';
+
+export function Statistics ({titel, data}) {
+   return <>
+    <section className={clsx(statCss.statistics)}>
+        {titel  && <h2 className={clsx(statCss.title)}>{titel}</h2>}
+
+
+        <ul className={clsx(statCss['stat-list'])}>
+            { data.map(el => (
+                <li key={el.id} className={clsx(statCss.item)}>
+                    <span className={clsx(statCss.label)}>{el.label}</span>
+                    <span className={clsx(statCss.percentage)}>{el.percentage}</span>
+                </li>
+            ))}
+        </ul>
+    </section>
+</>
+}
